d60227759bb605837a22f1bbd88e63889a1f94f8
nic-fisher <nic-fisher@outlook.com>
2024-02-05 20:14:22 +1000

    chapter 4 - testing

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
 
-mod vga_buffer;
-
-static HELLO: &[u8] = b"Hello World!";
-
+// this function is the entry point, since the linker looks for a function
+// named `_start` by default
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for(i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
-    //     }
-    // }
-
     println!("Hello World{}", "!");
 
-    panic!("some panic message");
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
 // This function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
