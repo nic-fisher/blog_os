b41d2e74b645695c5dae3a728f16a104698c4d4e
nic-fisher <nic-fisher@outlook.com>
2024-04-20 08:38:14 +1000

    chapter 6 - double faults

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,6 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-
     #[cfg(test)]
     test_main();
     println!("It did not crash");
